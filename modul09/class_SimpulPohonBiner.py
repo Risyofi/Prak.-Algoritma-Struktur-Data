83c1440fb4997de587cd48685b8363bdba7a951e
Risyofi <95907287+Risyofi@users.noreply.github.com>
2023-06-05 10:35:23 +0700

    Update class_SimpulPohonBiner.py

--- modul09/class_SimpulPohonBiner.py
+++ modul09/class_SimpulPohonBiner.py
@@ -4,3 +4,20 @@ class _SimpulPohonBiner(object):
         self.kiri = None
         self.kanan = None
         
+    def preorderTrav(subpohon):
+        if subpohon is not None:
+            print(subpohon.data)
+            preorderTrav(subpohon.kiri)
+            preorderTrav(subpohon.kanan)
+
+    def inorderTrav(subpohon):
+        if subpohon is not None:
+            inorderTrav(subpohon.kiri)
+            print(subpohon.data)
+            inorderTrav(subpohon.kanan)
+
+    def postorderTrav(subpohon):
+        if subpohon is note None:
+            postorderTrav(subpohon.kiri)
+            postorderTrav(subpohon.kanan)
+            print(subpohon.data)
