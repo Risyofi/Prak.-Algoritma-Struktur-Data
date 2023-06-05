3534cb2a969229839a4f134be24ded98979558c3
Risyofi <95907287+Risyofi@users.noreply.github.com>
2023-06-05 11:14:13 +0700

    Update Lat_9_4.py

--- modul09/Lat_9_4.py
+++ modul09/Lat_9_4.py
@@ -1,4 +1,4 @@
-from class_SimpulPohonBiner import _SimpulPohonBiner
+from class_SimpulPohonBiner import _SimpulPohonBiner, preorderTrav, inorderTrav, postorderTrav
 
 # Membuat simpul-simpul dan mengisi data
 a = _SimpulPohonBiner('Ambarawa')
@@ -12,29 +12,52 @@ h = _SimpulPohonBiner('Halmahera Timur')
 i = _SimpulPohonBiner('Indramayu')
 j = _SimpulPohonBiner('Jakarta')
 
+
 # Menghubungkan simpul ortu-anakk
+print("pohon biner a")
 a.kiri = b; a.kanan = c
 b.kiri = d; b.kanan = e
 c.kiri = f; c.kanan = g
 e.kiri = h
-g.kanan = i
+g.kiri = i; g.kanan = j
 
+print("preorderTrav : ")
+preorderTrav(a)
+print("\ninorderTrav : ")
+inorderTrav(a)
+print("\npostorderTrav : ")
+postorderTrav(a)
 
 # b
-print("pohon biner b")
+print("\npohon biner b")
 a.kiri = b; a.kanan = c
 b.kiri = d; b.kanan = e
+c.kanan = f
 e.kiri = g
 g.kanan = h
 h.kiri = i
-c.kanan = f
+
+
+print("preorderTrav : ")
+preorderTrav(a)
+print("\ninorderTrav : ")
+inorderTrav(a)
+print("\npostorderTrav : ")
+postorderTrav(a)
 
 # c
-print("pohon biner c")
+print("\npohon biner c")
 a.kanan = b
 b.kanan = c
 c.kiri = d
 d.kanan = e
 e.kiri = f
-f.kanan g
+f.kanan = g
 g.kiri = h
+
+print("preorderTrav : ")
+preorderTrav(a)
+print("\ninorderTrav : ")
+inorderTrav(a)
+print("\npostorderTrav : ")
+postorderTrav(a)
